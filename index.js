0878dbf846b8db8e7a1b36e33e0029bdf4b4823d
Murad Gazymagomedov <murad.dev@bk.ru>
2025-03-19 20:56:45 +0400

    practice

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
+function sum(a) {
+
+    let currentSum = a;
+
+    function f(b) {
+        currentSum += b;
+        return f;
+    }
+
+    f.toString = function () {
+        return currentSum;
+    };
+
+    return f;
+}
+
+alert(sum(1)(2)); // 3
+alert(sum(5)(-1)(2)); // 6
+alert(sum(6)(-1)(-2)(-3)); // 0
+alert(sum(0)(1)(2)(3)(4)(5)); // 15
+
+
+
+// function makeCounter() {
+//     let count = 0;
+
+//     function counter() {
+//         return count++;
+//     }
+
+//     counter.set = value => count = value;
+
+//     counter.decrease = () => count--;
+
+//     return counter;
+// }
+
+// Array.prototype.findUnique = function () {
+//     const result = [];
+
+//     for (let x = 0; x < this.length; x++) {
+//         let count = 0;
+//         for (let y = 0; y < this.length; y++) {
+//             if (this[x] === this[y]) {
+//                 count++;
+//             }
+//         }
+//         if(count === 1)
+//             result.push(this[x]);
+//     }
+
+//     return result;
+// }
+
+
+// const unique = [10, 5, 10, 0, 6, 6, 7, 2, 9, 9].findUnique(); //5, 0, 7, 2
+// console.log(unique)
+
+
 /* function countOccurences(arr) {
     const list = new Map();
     arr.forEach((el) => {
