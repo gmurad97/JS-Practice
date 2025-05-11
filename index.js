888105d88a3c134dfa9e38bf9794d804aae888d3
Murad Gazymagomedov <murad.dev@bk.ru>
2025-05-11 23:40:30 +0400

    js practice 2025

--- index.js
+++ index.js
@@ -1,113 +1,56 @@
-// "use strict";
+// bracket test js + FIFO or LIFO
 
-// let geom = {
-// 	name: "Фигура",
-// 	sp: { x: 0, y: 0 },
-// 	ep: { x: 100, y: 20 },
-// 	get nameGeom() { return this.name; },
-// 	set nameGeom(value) { this.name = value; }
-// };
+// /**
+//  * checkBrackets - check Brackets
+//  * @param {String} str 
+//  */
+// function checkBrackets(str) {
+// 	if (!str.length)
+// 		return true;
 
-// let rect = {
-// 	draw() {
-// 		console.log("Рисование прямоугольника: " +
-// 			this.sp.x + "," + this.sp.y + "," + this.ep.x + "," + this.ep.y);
-// 	}
-// };
+// 	brackets = {
+// 		"(": ")",
+// 		"[": "]",
+// 		"{": "}"
+// 	};
 
-// rect.__proto__ = geom;
+// 	stack = [];
 
-// console.log(rect.nameGeom);
-// rect.nameGeom = "Прямоугольник";
-// console.log(rect.nameGeom);
-// console.log(geom.nameGeom);
-
-// for (let prop in rect)
-// 	if (rect.hasOwnProperty(prop))
-// 		console.log(prop + ":" + rect[prop]);
-// let info = {
-// 	getInfo() {
-// 		console.log(this.name);
-// 	},
-// 	__proto__: rect
-// };
-
-// rect.draw();
-// info.getInfo();
-
-// let x = 10_000_000;
-// const map = new Map();
-// const set = new Set();
-// const arr = [];
-
-// console.time("start");
-
-// for(let y = 0; y<=x; y++){
-// 	arr.push(y);
-// }
-
-// console.timeEnd("start");
-
-// function range(arr) {
-// 	if (!arr.length) {
-// 		return "";
-// 	}
-
-// 	const sortedArr = [...arr].sort((a, b) => a - b);
-
-// 	let result = [];
-// 	let start = sortedArr[0];
-// 	let end = start;
-
-// 	for (let x = 1; x <= sortedArr.length; x++) {
-// 		if (sortedArr[x] === end + 1) {
-// 			end = sortedArr[x];
+// 	for (let idx = 0; idx < str.length; idx++) {
+// 		const bracket = str[idx];
+// 		if (brackets[bracket]) {
+// 			stack.push(bracket);
 // 		}
-// 		else {
-// 			if (start === end) {
-// 				result.push(`${start}`);
+// 		else if (Object.values(brackets).includes(bracket)) {
+// 			if (stack.length === 0)
+// 				return false;
+// 			const top = stack.pop();
+// 			if (brackets[top] !== bracket) {
+// 				return false;
 // 			}
-// 			else {
-// 				result.push(`${start}-${end}`);
-// 			}
-// 			start = sortedArr[x];
-// 			end = start;
 // 		}
 // 	}
-
-// 	return result.join(",");
+// 	return stack.length === 0;
 // }
 
-// console.log(range([1, 4, 5, 2, 3, 9, 8, 11, 0])); // "0-5,8-9,11"
-// console.log(range([1, 4, 3, 2])); // "1-4"
+// console.log(checkBrackets("{{(([}](}}")); // false
+// console.log(checkBrackets("(){}[]"));     // true
+// console.log(checkBrackets("{[()()][]}")); // true
+// console.log(checkBrackets("((()))"));     // true
+// console.log(checkBrackets("([)]"));       // false
+// console.log(checkBrackets("{[}"));        // false
+// =============================================================================
+
+function retryFetch(url, { retryCount, timeout }) {
+
+}
+
+console.log(retryFetch(
+	"https://jsonplaceholder.typicode.com/users",
+	{ retryCount: 10, timeout: 1000 }
+));
 
 
-/*
-2. Дан массив целых неотрицательных чисел, нужно группировать друг с другом числа,
-	которые можно получить путём перестановки цифр их составляющих.
-	Нули при этом игнорируем, т.к. нет числа 011.
-	Решение должно быть максимально эффективно по памяти и времени.
-*/
 
-// function digitPermutation(arr) {
-// 	const groups = new Map();
 
-// 	for (let idx = 0; idx < arr.length; idx++) {
-// 		const currentValue = parseInt(arr[idx].toString().replaceAll("0", "").split("").sort().join(""));
-// 		if (groups.has(currentValue)) {
-// 			groups.get(currentValue).push(arr[idx]);
-// 		}
-// 		else {
-// 			groups.set(currentValue, [arr[idx]]);
-// 		}
-// 	}
 
-// 	return Array.from(groups.values());
-// }
-
-// console.log('start test');
-// console.log(digitPermutation([123, 99, 23001, 123, 111, 300021, 101010, 90000009, 9]));
-// // [[99, 90000009], [111, 101010], [123, 123, 23001, 300021], [9]]
-// console.log(digitPermutation([111, 221, 1])); // [[111], [221], [1]]
-// console.log(digitPermutation([11111111112, 12222222222])); // [[11111111112], [12222222222]]
-// console.log('end test');
