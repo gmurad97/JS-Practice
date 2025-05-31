5f92abb0e99d9e9390244fc831c6c33600883b97
Murad Gazymagomedov <murad.dev@bk.ru>
2025-05-31 23:57:28 +0400

    algo training

--- index.js
+++ index.js
@@ -89,4 +89,18 @@
 
 // console.log(bubbleSort(testArray));
 // =============================================================================
+//Recursion
+// function factorial(n) {
+//     if (n === 1)
+//         return 1;
+//     return n * factorial(n - 1);
+// }
+// console.log(factorial(6));
+
+// function fibonachi(n) {
+//     if (n === 1 || n === 2) return 1;
+//     return fibonachi(n - 1) + fibonachi(n - 2);
+// }
+// console.log(fibonachi(8));
+// =============================================================================
 
